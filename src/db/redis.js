db607caa15371645d31728b04814dd14d7bdb3e4
POABOB <38785340+POABOB@users.noreply.github.com>
2022-03-23 02:48:58 +0800

    redis修改

--- src/db/redis.js
+++ src/db/redis.js
@@ -4,18 +4,36 @@ const { REDIS_CONF } = require('../config/db');
 // 生成redis的client
 const client = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
 client.connect();
+client.on('error', err => {
+	console.log(err);
+});
 module.exports = {
 	// 存储值
 	set: set = (key, val) => {
-		if(typeof val === 'object') {
-			val = JSON.stringify(val);
-		}
-		client.set(key, val)
+		const p = new Promise((resolve, reject) => {
+			if(typeof val === 'object') {
+				val = JSON.stringify(val);
+			}
+	
+			client.set(key, val, (err, res) => {
+				if(err) {
+					reject(err);
+					return;
+				}
+	
+				try {
+					resolve(JSON.parse(res));
+				} catch(ex) {
+					resolve(res);
+				}
+			});
+		});
+		return p;
 	},
  
 	// 获取string
 	get: get = (key) => {
-		return new Promise((resolve, reject) => {
+		const p = new Promise((resolve, reject) => {
 			client.get(key, (err, val) => {
 				if (err) {
 					reject(err);
@@ -32,5 +50,6 @@ module.exports = {
 				}
 			});
 		});
+		return p;
 	}
 }
