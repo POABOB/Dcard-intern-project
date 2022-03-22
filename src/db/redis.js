0f0d44ca028a9bff0591dfe0abf7fe566b2fde2a
POABOB <38785340+POABOB@users.noreply.github.com>
2022-03-23 02:36:01 +0800

    redis修改

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,42 +1,34 @@
 const redis = require('redis');
 const { REDIS_CONF } = require('../config/db');
 
+// 生成redis的client
+const client = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+
 module.exports = {
-	config: REDIS_CONF,
-	client: null,
-	create: function () {
-		if(!this.client) {
-			this.client = redis.createClient(this.config.port, this.config.host);
-			this.client.connect();
-			this.client.on('error', err => {console.log('Error ' + err);});
-		}
-	},
-	set: async function (key, val) {
-		this.create();
+	// 存储值
+	set: set = (key, val) => {
 		if(typeof val === 'object') {
 			val = JSON.stringify(val);
 		}
-
-		this.client.set(key, val);
+		client.set(key, val)
 	},
-	get: async function (key) {
+ 
+	// 获取string
+	get: get = (key) => {
 		return new Promise((resolve, reject) => {
-				this.create();
-
-				this.client.get(key, (err, val) => {
-				if(err) {
+			client.get(key, (err, val) => {
+				if (err) {
 					reject(err);
-					return;
-				}
-	
-				if(val === null) {
-					resolve(null);
-					return;
-				}
-				try {
-					resolve(JSON.parse(val));
-				} catch (err) {
-					reject(err);
+				}else{
+					if(val === null) {
+							resolve(null);
+							return;
+					}
+					try {
+						resolve(JSON.parse(val));
+					} catch (err) {
+						reject(err);
+					}
 				}
 			});
 		});
