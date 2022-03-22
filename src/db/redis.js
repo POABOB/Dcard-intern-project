afdd708b10c0e7accbe4d50b66eb2f103fb888ef
POABOB <38785340+POABOB@users.noreply.github.com>
2022-03-23 02:15:55 +0800

    修改redis

--- src/db/redis.js
+++ src/db/redis.js
@@ -7,35 +7,21 @@ module.exports = {
 	create: function () {
 		if(!this.client) {
 			this.client = redis.createClient(this.config.port, this.config.host);
+			this.client.connect();
+			this.client.on('error', err => {console.log('Error ' + err);});
 		}
-		this.client.on('error', err => {console.log('Error ' + err);});
 	},
 	set: async function (key, val) {
-		return new Promise(( resolve, reject ) => {
-			this.create();
-			// this.client.connect();
-			
-			if(typeof val === 'object') {
-				val = JSON.stringify(val);
-			}
+		this.create();
+		if(typeof val === 'object') {
+			val = JSON.stringify(val);
+		}
 
-			this.client.set(key, val, (err, res) => {
-				if(err) {
-					reject(err);
-					return;
-				}
-				try {
-					resolve(res);
-				} catch (err) {
-					reject(err);
-				}
-			});
-		});
+		this.client.set(key, val);
 	},
 	get: async function (key) {
 		return new Promise((resolve, reject) => {
 				this.create();
-				// this.client.connect();
 
 				this.client.get(key, (err, val) => {
 				if(err) {
